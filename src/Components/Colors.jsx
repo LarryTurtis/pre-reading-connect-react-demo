bcd9b99783f0f0ae5d8b1be4910ee33b8579c981
Gary Kertis <Gary>
2023-10-12 11:02:31 -0400

    Add index route.

--- src/Components/Colors.jsx
+++ src/Components/Colors.jsx
@@ -1,10 +1,17 @@
-import { useState, useEffect } from "react";
-import Color from "./Color";
+import { useState, useEffect } from 'react';
+import Color from './Color';
 
 const API = import.meta.env.VITE_API_URL;
 
 function Colors() {
   const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/colors`)
+      .then((response) => response.json())
+      .then((responseJSON) => setColors(responseJSON))
+      .catch((error) => console.error(error));
+  });
 
   return (
     <div className="Colors">
